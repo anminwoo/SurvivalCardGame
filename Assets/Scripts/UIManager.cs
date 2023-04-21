92905975c32ad68ea3cdb2926310d9e738a83e70
anminwoo <minwoo5995@gmail.com>
2023-04-21 09:17:55 +0900

    Add Change player stats function

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,9 +6,16 @@ using UnityEngine;
 public class UIManager : MonoBehaviour
 {
     public TextMeshProUGUI dayText;
+
+    public Bar[] stateBars;
     
     public void ChangeDayText(int day)
     {
         dayText.text = $"Day: {day}";
+    }
+
+    public void ChangeStat(PlayerStat type, int amount)
+    {
+        stateBars[(int)type].Change(amount);
     }
 }
