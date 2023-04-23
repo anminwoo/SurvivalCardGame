33481e0bcd3e03c1446657d1e34a6a439f3bf7e0
anminwoo <minwoo5995@gmail.com>
2023-04-23 20:00:04 +0900

    Add cardpack name

--- Assets/Scripts/Legacy/CardPack.cs
+++ Assets/Scripts/Legacy/CardPack.cs
@@ -5,5 +5,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "CardPackName", menuName = "CardPack")]
 public class CardPack : ScriptableObject
 {
+    public string cardPackName;
     public List<CardData> cardPack;
 }
