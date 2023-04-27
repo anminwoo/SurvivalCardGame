ad28966b512e4c200b2603971c6fcc81b4cc1ebe
anminwoo <minwoo5995@gmail.com>
2023-04-27 23:50:30 +0900

    Add Tooltips

--- Assets/Scripts/Legacy/CardData.cs
+++ Assets/Scripts/Legacy/CardData.cs
@@ -7,11 +7,13 @@ using UnityEngine.UI;
 [CreateAssetMenu(fileName = "CardName", menuName = "CardData")]
 public class CardData : ScriptableObject
 {
+    [Tooltip("카드 선택지 이름입니다. 카드를 찾을 때 사용하므로 중복이 불가합니다.")]
     public string cardName;
     
-    
+    [TextArea]
     public string explanation;
     public Image characterImage;
+    [Tooltip("카드 이미지의 이름입니다. 중복이 가능합니다.")]
     public string characterName;
     public string leftDialogue;
     public string rightDialogue;
